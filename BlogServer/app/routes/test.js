fd10774750a3f534b7e7d947c726a06dd422a2ba
CAaRrLl <1622452328@qq.com>
2018-03-28 22:26:28 +0800

    添加日志文件夹，静态文件文件夹

--- BlogServer/app/routes/test.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/api/frontlog', function(req, res, next) {
-    console.log(res,req)
-  });
-  
-  module.exports = router;
